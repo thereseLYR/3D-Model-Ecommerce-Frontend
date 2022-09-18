1babfcf4b2a7119859202cb7ccd17272eb4e9b72
Nyx92 <ngyx92@gmail.com>
2022-09-19 00:19:09 +0800

    login, signup pages, cookies, privateRoute validations

---
+++ src/components/BackendUrl.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react';
+
+const BackendUrlContext = createContext();
+
+export function BackendUrlProvider({ children, backendUrlData }) {
+  return (
+    <BackendUrlContext.Provider value={{ backendUrl: backendUrlData }}>
+      {children}
+    </BackendUrlContext.Provider>
+  );
+}
+
+export default BackendUrlContext;
