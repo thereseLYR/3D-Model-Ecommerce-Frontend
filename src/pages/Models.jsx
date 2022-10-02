21f843f0f99dbc32f6fbc5c103dc64f2a6ec57b0
Felicia Tan <felicia.tan@circles.asia>
2022-10-02 17:53:03 +0800

    feat: created basic navbar and model box components as well as basic landing page

---
+++ src/pages/Models.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useContext } from 'react'
+import { Heading, Box, Grid, GridItem, Text } from '@chakra-ui/react'
+import Navbar from '../components/Navbar.jsx'
+import ModelBox from '../components/ModelBox.jsx'
+
+export default function Models() {
+  return (
+    <>
+      <Navbar />
+      <Grid p={4}>
+        <Text>
+          Incomplete page. Supposed to have sidenav at the left with all the
+          categories, and display of the models on the right.
+        </Text>
+      </Grid>
+    </>
+  )
+}
