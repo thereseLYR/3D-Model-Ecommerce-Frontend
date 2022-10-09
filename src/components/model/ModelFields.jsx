94576420fb7f49ed14a45098bb2a69743eea670b
staceyng <stacey.ng@motional.com>
2022-10-09 14:31:50 +0800

    feat: add static model landing page

---
+++ src/components/model/ModelFields.jsx
@@ -0,0 +1,210 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
+
+// TODO: get this model from db, model can consist of multiple parts, we keep track of them in component_breakdown
+const defaultModel = {
+  id: 1,
+  model_name: "Clicky",
+  model_description: "some description",
+  ppu: 30,
+  component_breakdown: {
+    part_id_1: { part_name: "Gear", default_color: "grey", color: null }, // color change in configurator to update this field in cookies
+    part_id_2: {
+      part_name: "Front Housing",
+      default_color: "green",
+      color: null,
+    },
+    part_id_3: {
+      part_name: "Back Housing",
+      default_color: "green",
+      color: null,
+    },
+  },
+  quantity: 0,
+  material: "",
+};
+
+export default function ModelFields() {
+  const [material, setMaterial] = useState("PLA");
+  const [quantity, setQuantity] = useState(0);
+  const [successfulAddCart, setSuccessfulAddCart] = useState(false);
+  const [cookies, setCookie] = useCookies(["temp-cart"]);
+
+  const handleAddToCartClick = () => {
+    const componentBreakDownCopy = { ...defaultModel.component_breakdown };
+    // set colors
+    for (const p in componentBreakDownCopy) {
+      componentBreakDownCopy[p]["color"] = componentBreakDownCopy[p].color
+        ? componentBreakDownCopy[p].color
+        : componentBreakDownCopy[p].default_color;
+    }
+
+    const cartModel = {
+      ...defaultModel,
+      component_breakdown: componentBreakDownCopy,
+      quantity: quantity,
+      material: material,
+    };
+
+    console.log(cartModel);
+
+    // add updated cartModel in cookies
+    setCookie(defaultModel.model_name, cartModel, { path: "/temp-cart" });
+    setSuccessfulAddCart(true);
+  };
+
+  const handleOnMaterialChange = (ev) => {
+    console.log("material change event: ", ev.target.value);
+    setMaterial(ev.target.value);
+  };
+
+  const handleOnQuantityChange = (ev) => {
+    console.log("quantity change event: ", ev);
+    setQuantity(ev);
+  };
+
+  const ModelBreakdown = () => {
+    return (
+      <Box>
+        <Text
+          color={"gray.500"}
+          textTransform={"uppercase"}
+          fontWeight={800}
+          fontSize={"md"}
+          letterSpacing={1.1}
+          textAlign={"left"}
+        >
+          Clicky Component Breakdown
+        </Text>
+        <TableContainer>
+          <Table variant="striped" colorScheme="teal">
+            <TableCaption>
+              All parts will be made in the same material
+            </TableCaption>
+            <Thead>
+              <Tr>
+                <Th>No.</Th>
+                <Th>Part Name</Th>
+                <Th>Color</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {Object.keys(defaultModel.component_breakdown).map((m, i) => (
+                <Tr key={i}>
+                  <Td>{i + 1}</Td>
+                  <Td>{defaultModel.component_breakdown[m].part_name}</Td>
+                  <Td>
+                    {defaultModel.component_breakdown[m].color
+                      ? defaultModel.component_breakdown[m].color
+                      : defaultModel.component_breakdown[m].default_color}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    );
+  };
+
+  return (
+    <VStack alignItems={"start"} spacing={5}>
+      <Text
+        color={"gray.700"}
+        fontWeight={800}
+        textTransform={"uppercase"}
+        fontSize={"4xl"}
+      >
+        Clicky
+      </Text>
+      <Text
+        color={"green.500"}
+        textTransform={"uppercase"}
+        fontWeight={800}
+        fontSize={"lg"}
+        letterSpacing={1.1}
+        textAlign={"left"}
+      >
+        Customise color, material and quantity
+      </Text>
+      <ModelBreakdown />
+      <FormControl>
+        <FormLabel
+          color={"gray.500"}
+          textTransform={"uppercase"}
+          fontWeight={800}
+          fontSize={"md"}
+          letterSpacing={1.1}
+          textAlign={"left"}
+        >
+          Material
+        </FormLabel>
+        <Select
+          defaultValue={material}
+          placeholder="Select a material"
+          onChange={handleOnMaterialChange}
+        >
+          <option value="PLA">PLA</option>
+          <option value="ABS">ABS</option>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <FormLabel
+          color={"gray.500"}
+          textTransform={"uppercase"}
+          fontWeight={800}
+          fontSize={"md"}
+          letterSpacing={1.1}
+          textAlign={"left"}
+        >
+          Quantity
+        </FormLabel>
+        <NumberInput
+          defaultValue={0}
+          min={1}
+          max={5}
+          onChange={handleOnQuantityChange}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+      <br />
+      <Button
+        colorScheme="pink"
+        variant="solid"
+        size="lg"
+        onClick={handleAddToCartClick}
+        disabled={quantity === 0}
+      >
+        Add to Cart
+      </Button>
+      <Text>{successfulAddCart && "Successfully added to cart!"}</Text>
+    </VStack>
+  );
+}
---
+++ src/components/model/StaticModel.jsx
@@ -0,0 +1,69 @@
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Button,
+  Container,
+  HStack,
+  Image,
+  StackDivider,
+  VStack,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import ModelFields from "./ModelFields";
+import ModelTabs from "./ModelTabs";
+
+export default function StaticModel() {
+  const navigate = useNavigate();
+
+  const boxWidth = "500px";
+  const boxHeight = "700px";
+
+  const handleCustomiseModelClick = () => {
+    navigate("/clicky");
+  };
+
+  return (
+    <Container>
+      <VStack spacing={10}>
+        <Box alignItems={"left"}>
+          <Breadcrumb fontWeight="medium" fontSize="sm">
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/models">3D Models</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem isCurrentPage>
+              <BreadcrumbLink href="/model">Clicky</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Box>
+        <Box>
+          <HStack spacing={5} divider={<StackDivider borderColor="gray.200" />}>
+            <Box w={boxWidth} h={boxHeight}>
+              <VStack spacing={3}>
+                <Image src="https://picsum.photos/400" alt="product_image" />
+                <Button
+                  colorScheme="teal"
+                  variant="outline"
+                  size="lg"
+                  onClick={handleCustomiseModelClick}
+                >
+                  Customise Model
+                </Button>
+              </VStack>
+            </Box>
+            <Box w={boxWidth} h={boxHeight}>
+              <ModelFields />
+            </Box>
+          </HStack>
+        </Box>
+        <Box>
+          <ModelTabs />
+        </Box>
+      </VStack>
+    </Container>
+  );
+}
