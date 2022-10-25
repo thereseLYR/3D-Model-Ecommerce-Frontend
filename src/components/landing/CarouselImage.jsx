6cc3e1420446788d85c8384e07a916c01fc26d41
staceyng <stacey.ng@motional.com>
2022-10-25 14:07:17 +0800

    fix: landing vp

--- src/components/landing/CarouselImage.jsx
+++ src/components/landing/CarouselImage.jsx
@@ -4,7 +4,7 @@ export default function CarousellImage(props) {
   const { srcPath } = props;
   return (
     <>
-      <Box w="38vw">
+      <Box w="30vw">
         <Image h="100%" src={srcPath} alt="a 3D Model" objectFit={"cover"} />
       </Box>
     </>
